7eba4c1f17192df4f949149631482adf7c9f4b75
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2019-06-28 23:58:13 +0500

    added priliminary structure for setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
-# setup.py file
+from setuptools import setup
+
+setup(
+    name='neat',
+    version='0.1',
+    author='elcid, bazarovay',
+    author_email='syedmohsinbukhari@googlemail.com',
+    maintainer='Syed Mohsin Bukhari, Rafid Saad',
+    maintainer_email='syedmohsinbukhari@googlemail.com',
+    url='https://github.com/syedmohsinbukhari/neat',
+    license="MIT",
+    description='NEAT (NeuroEvolution of Augmenting Topologies) Python3 implementation',
+    long_description='NeuroEvolution of Augmenting Topologies (NEAT), which at the time of publication ' +
+                     'outperformed the best fixed-topology method on a challenging ' + 
+                     'benchmark reinforcement learning task.',
+    packages=['neat'],
+    classifiers=[]
+)
