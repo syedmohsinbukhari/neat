250594d31e9302c05a97be697833cc068a079731
Bazarovay <rxs6754@rit.edu>
2019-07-10 23:36:43 +0500

    feed forward

--- experiments/backprop/activation.py
+++ experiments/backprop/activation.py
@@ -25,3 +25,23 @@ def gauss_activation(z):
 
 def relu_activation(z):
 return z if z > 0.0 else 0.0
+
+
+
+class Forward():
+    
+    def activate(self, inputs):
+        if len(self.input_nodes) != len(inputs):
+            raise RuntimeError("Expected {0:n} inputs, got {1:n}".format(len(self.input_nodes), len(inputs)))
+
+        for k, v in zip(self.input_nodes, inputs):
+            self.values[k] = v
+
+        for node, act_func, agg_func, bias, response, links in self.node_evals:
+            node_inputs = []
+            for i, w in links:
+                node_inputs.append(self.values[i] * w)
+            s = agg_func(node_inputs)
+            self.values[node] = act_func(bias + response * s)
+
+        return [self.values[i] for i in self.output_nodes]
