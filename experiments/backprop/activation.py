46fcb5c36be2719ebcce725f7ddb72a5ed4ffab6
_ <Bazarovay@users.noreply.github.com>
2019-07-15 23:48:08 +0500

    Update activation.py

--- experiments/backprop/activation.py
+++ experiments/backprop/activation.py
@@ -2,32 +2,6 @@ from __future__ import division
 import math
 import types
 
-
-def sigmoid_activation(z):
-    z = max(-60.0, min(60.0, 5.0 * z))
-    return 1.0 / (1.0 + math.exp(-z))
-
-
-def tanh_activation(z):
-    z = max(-60.0, min(60.0, 2.5 * z))
-    return math.tanh(z)
-
-
-def sin_activation(z):
-    z = max(-60.0, min(60.0, 5.0 * z))
-    return math.sin(z)
-
-
-def gauss_activation(z):
-    z = max(-3.4, min(3.4, z))
-    return math.exp(-5.0 * z ** 2)
-
-
-def relu_activation(z):
-return z if z > 0.0 else 0.0
-
-
-
 class Forward():
     
     def activate(self, inputs):
