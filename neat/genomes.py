e57897e5958d8af89c90f46cad3b5ff6cf412883
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2019-07-26 13:16:41 +0500

    Added Genome class along with its attributes and methods

--- neat/genomes.py
+++ neat/genomes.py
@@ -3,6 +3,14 @@ Description: Genomes classes
 author: syedmohsinbukhari@googlemail.com
 """
 
-class Genome:
-    pass
 
+class Genome:
+    def __init__(self):
+        self.nodes = []
+        self.connections = []
+
+    def get_node_count(self):
+        return len(self.nodes)
+
+    def get_connection_count(self):
+        return len(self.connections)
