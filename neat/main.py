e05656d410ca2a73c9beb3e423fe61ccc5ac72f5
Bazarovay <rafid.binsaad@systemsltd.com>
2019-07-26 13:35:16 +0500

    check code doc generation

---
+++ neat/main.py
@@ -0,0 +1,23 @@
+"""
+author: Bazarovay@github.com
+This is the primary class
+"""
+import numpy as np
+
+
+def main(test_argument):
+    """
+    To test the documentation
+    Args:
+        test_argument:
+
+    Returns:
+
+    """
+    product = np.multiply([[1]],[[1]])
+    print(test_argument, product)
+    return None
+
+
+if __name__ == "__main__":
+    main("test_argument")
